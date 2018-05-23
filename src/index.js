491ac4ca3fb47f1cbf28d07f2efe83b848745ec6
Milan Košir <54502+kosirm@users.noreply.github.com>
2018-05-23 23:00:56 +0200

    Fetch ne radi

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const App = () => (
     </h2>
     <h3>+ snack, da vidim kako ide tamo cerebral</h3>
     <h3>... a onda dolazi na red pagedraw</h3>
+    <h3>... ok, fetching ne radi kako treba...</h3>
   </div>
 );
 
